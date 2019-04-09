b21478477583a49eea4e1bc47325d8c6d6b8af1b
Sang Gwon Cha <ck2570@naver.com>
2019-04-09 02:10:01 +0000

    이미지 업로드 구현

--- posts/models.py
+++ posts/models.py
@@ -1,6 +1,14 @@
 from django.db import models
+from imagekit.models import ProcessedImageField
+from imagekit.processors import ResizeToFill
 
 # Create your models here.
 class Post(models.Model):
     content = models.CharField(max_length=100)
-    
+    # image = models.ImageField(blank=True)
+    image = ProcessedImageField(
+                upload_to = 'posts/images', # 저장위치
+                processors = [ResizeToFill(600,600)], # 크기지정
+                format = 'JPEG',
+                options = {'quality':90},
+        )
