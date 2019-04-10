1b0788ced2dfad8956719af9228b557ae9a37bb6
Sang Gwon Cha <ck2570@naver.com>
2019-04-10 06:07:18 +0000

    유저확인시키기

--- posts/models.py
+++ posts/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 from imagekit.models import ProcessedImageField
 from imagekit.processors import ResizeToFill
+from django.conf import settings
 
 # Create your models here.
 class Post(models.Model):
     content = models.CharField(max_length=100)
     # image = models.ImageField(blank=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    
 class Image(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE) # 상위 파일이 지워지면 같이 지워지게 만듦, on_delete=models.CASCADE
     file = ProcessedImageField(
