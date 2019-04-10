ad261e138bb4f5c71931fc360e1456e535b38776
Sang Gwon Cha <ck2570@naver.com>
2019-04-10 04:37:57 +0000

    login_required

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm # 인증&권한:AuthenticationForm
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
+
 # Create your views here.
 def signup(request):
     if request.method == "POST": # 저장
@@ -10,4 +13,19 @@ def signup(request):
         
     else : # 입력할 수 있는 폼
         form = UserCreationForm()
-    return render(request,"accounts/signup.html",{"form":form})
+    return render(request,"accounts/form.html",{"form":form})
+    
+def login(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request,request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            return redirect("posts:list")
+    else :
+        form = AuthenticationForm()
+    
+    return render(request,"accounts/form.html", {"form":form})
+
+def logout(request):
+    auth_logout(request)
+    return redirect("posts:list")
