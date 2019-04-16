9ea7ba92d13580bfc20bddb6dae95ed01470b6b2
Sang Gwon Cha <ck2570@naver.com>
2019-04-16 01:13:17 +0000

    프로필 수정 기능 추가

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,7 +1,15 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import User
 
 class CustomUserCreationFrom(UserCreationForm):
     class Meta(UserCreationForm):
         model = User
         fields = ['username','email']
+        
+class CustomUserChangeFrom(UserChangeForm):
+    password = None 
+    # UserChangeForm의 password 칼럼에 아무것도 안넣음
+    
+    class Meta:
+        model = User
+        fields = ['email',]
--- accounts/views.py
+++ accounts/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm # 인증&권한:AuthenticationForm
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
-from .forms import CustomUserCreationFrom
+from .forms import CustomUserCreationFrom, CustomUserChangeFrom
 from django.contrib.auth import get_user_model
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def signup(request):
@@ -37,6 +38,28 @@ def user_page(request,id):
     user_info = User.objects.get(id=id)
     return render(request, "accounts/user_page.html",{"user_info":user_info})
     
+@login_required 
+def edit_profile(request,id):
+    me = request.user
+    User = get_user_model()
+    user = User.objects.get(id=id)
+    if me == user :
+        if request.method == "POST":
+            form = CustomUserChangeFrom(request.POST, instance=user)
+            # form = CustomUserChangeFrom(instance=user, data = request.POST)
+            # user에 대한 정보를 instance 안에 넣어줘야함(수정하기 위함)
+            if form.is_valid() :
+                form.save()
+                return redirect('accounts:user_page', id)
+        else :
+            form = CustomUserChangeFrom(instance=user)
+        return render(request, 'accounts/form.html',{'form':form})
+    else :
+        return redirect('posts:list')
+    
+    
+
+@login_required
 def follow(request,id):
     User = get_user_model()
     me = request.user # 로그인한 사람
